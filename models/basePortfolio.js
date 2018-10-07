aa298fa8bcf6c96af540bed572f314ebeca48853
Anurag Rai <7anuragrai7@gmail.com>
2018-10-07 20:27:58 +0530

    Adding base portfolio

---
+++ models/basePortfolio.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const BasePortfolio = mongoose.model('BasePortfolio', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  stocks: { 
+    type: [String],  
+    required: true
+  },
+}));
+
+function validateBasePortfolio(basePortfolio) {
+  const schema = {
+    name: Joi.string().trim().min(5).max(255).required(),
+    stocks: Joi.array().items(Joi.string()).min(1).unique().required()
+  };
+
+  return Joi.validate(basePortfolio, schema);
+}
+
+function validateBasePortfolioForPut(basePortfolio) {
+  const schema = Joi.object().keys({
+    name: Joi.string().trim().min(5).max(255),
+    stocks: Joi.array().items(Joi.string()).min(1).unique()
+  }).or('name','stocks');
+
+  return Joi.validate(basePortfolio, schema);
+}
+
+
+exports.BasePortfolio = BasePortfolio; 
+exports.validate = validateBasePortfolio;
+exports.validateBasePortfolioForPut = validateBasePortfolioForPut;
---
+++ routes/basePortfolios.js
@@ -0,0 +1,121 @@
+const validateObjectId = require('../middleware/validateObjectId');
+// const auth = require('../middleware/auth');
+// const admin = require('../middleware/admin');
+
+const {BasePortfolio, validate, validateBasePortfolioForPut} = require('../models/basePortfolio');
+const {Stock} = require('../models/stock');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req, res) => {
+  const basePortfolios = await BasePortfolio.find().sort('name');
+  res.send(basePortfolios);
+});
+
+
+// Add a new basePortfolio.
+// TODO: add auth middleware for authorization
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Name has to be unique
+  let valid = await isValidName(req.body.name)
+  if ( !valid ) {
+    return res.status(400).send('The basePortfolio with the given Name already exists'); //TODO: 400 Bad Request?
+  }
+  valid = await areStocksValid(req.body.stocks)
+  // Stocks have to be valid
+  if ( !valid ) {
+    return res.status(400).send('The basePortfolio does not have valid stocks');
+  }
+
+  basePortfolio = new BasePortfolio({
+    name: req.body.name,
+    stocks: req.body.stocks
+  });
+  basePortfolio = await basePortfolio.save();
+  
+  res.send(basePortfolio);
+});
+
+
+//TODO: Add middleware [auth, validateObjectId]
+router.put('/:id', validateObjectId, async (req, res) => {
+  const { error } = validateBasePortfolioForPut(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // TODO: Develop better logic than to check param and update.
+  let basePortfolio;
+  if ( req.body.stocks ) {
+    // check if the new stocks are valid
+    let valid = await areStocksValid(req.body.stocks)
+    // Stocks have to be valid
+    if ( !valid ) {
+      return res.status(400).send('The new basePortfolio does not have valid stocks');
+    }
+    basePortfolio = await BasePortfolio.findByIdAndUpdate(req.params.id, {stocks: req.body.stocks}, {
+      new: true
+    });
+  }
+  if ( req.body.name ) {
+    basePortfolio = await BasePortfolio.findByIdAndUpdate(req.params.id, {name: req.body.name}, {
+      new: true
+    });
+  }
+  if (!basePortfolio) return res.status(404).send('The basePortfolio with the given ID was not found.');
+  
+  res.send(basePortfolio);
+});
+
+//TODO: Add middleware [auth, admin, validateObjectId]
+router.delete('/:id', validateObjectId, async (req, res) => {
+  const basePortfolio = await BasePortfolio.findByIdAndRemove(req.params.id);
+  if (!basePortfolio) return res.status(404).send('The basePortfolio with the given ID was not found.');
+  
+  res.send(basePortfolio);
+});
+
+
+//TODO: Add middleware validateObjectId
+router.get('/:id', validateObjectId, async (req, res) => {
+  const basePortfolio = await BasePortfolio.findById(req.params.id);
+  if (basePortfolio.length === 0) return res.status(404).send('The basePortfolio with the given Name was not found.');
+  
+  res.send(basePortfolio);
+});
+
+
+async function isValidName(name) {
+    let basePortfolio = await BasePortfolio.find({name: name});
+    if (basePortfolio.length !== 0) {
+      return false;
+    }
+    return true;
+}
+
+async function areStocksValid(stocks) {
+    console.log("Stocks: ", stocks);
+    const l = stocks.length;
+    var i;
+    var valid;
+    for(i = 0; i < l; i++ ) {
+      valid = await isStockValid(stocks[i]);
+      if ( !valid )
+        return false;
+    }
+    return true;
+}
+
+async function isStockValid(stockName) {
+    console.log("Stock: ", stockName);
+    const stock = await Stock.find({name: stockName});
+    console.log("Found: ", stock);
+    if (stock.length === 0)
+      return false;
+    return true
+}
+
+module.exports = router;
