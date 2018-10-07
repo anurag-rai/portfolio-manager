1acd0bb6dab5b1a09a60f71def67c812dc4614b3
Anurag Rai <7anuragrai7@gmail.com>
2018-10-07 20:56:19 +0530

    Adding winston for logging

--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const stocks = require('../routes/stocks');
 const trades = require('../routes/trades');
-const baseportfolio = require('../routes/basePortfolios');
+const basePortfolios = require('../routes/basePortfolios');
+const portfolios = require('../routes/portfolios');
 const error = require('../middleware/error');
 
 module.exports = function(app) {
   app.use(express.json());
   app.use('/api/stocks', stocks);
   app.use('/api/trades', trades);
-  app.use('/api/baseportfolio', baseportfolio);
+  app.use('/api/basePortfolios', basePortfolios);
+  app.use('/api/portfolios', portfolios);
   app.use(error);
 }
